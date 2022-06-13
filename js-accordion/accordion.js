4582c7491013a551db2b365d0b594b8a112f919f
seenaaa <manu20sena@gmail.com>
2022-06-13 21:24:23 +0900

    バグ修正

--- js-accordion/accordion.js
+++ js-accordion/accordion.js
@@ -4,22 +4,25 @@
         //初期化
         constructor(){
             const $elm = document.querySelector('#js-accordion');
+
             const $trigger = $elm.getElementsByTagName('a');
-        
+
             const triggerLen = $trigger.length;
             let index = 0;
             while (index < triggerLen) {
-                $trigger[index].addEventListener('click',(e) => clickHandler(e));
+                $trigger[index].addEventListener('click', (e) => this.clickHandler(e));
                 index++;
+                console.log(index)
             }
         }
+
         //クリックしたら実行される処理
         clickHandler(e){
             e.preventDefault();
     
             const $target = e.currentTarget;
             const $content = $target.nextElementSibling;
-            
+
             if($content.style.display === 'block'){
                 $content.style.display = 'none';
             } else {
@@ -32,8 +35,5 @@
         hookName: ''
     });
 
-
-
- 
-
+    fuckingAccordion.hoge()
 })();
