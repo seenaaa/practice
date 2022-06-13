0ebe0b1dfa667f65a40bfdcac0f6e356200bf705
seenaaa <manu20sena@gmail.com>
2022-06-13 21:01:46 +0900

    追加

---
+++ js-accordion/accordion.js
@@ -0,0 +1,39 @@
+(() =>{
+
+    class Accordion {
+        //初期化
+        constructor(){
+            const $elm = document.querySelector('#js-accordion');
+            const $trigger = $elm.getElementsByTagName('a');
+        
+            const triggerLen = $trigger.length;
+            let index = 0;
+            while (index < triggerLen) {
+                $trigger[index].addEventListener('click',(e) => clickHandler(e));
+                index++;
+            }
+        }
+        //クリックしたら実行される処理
+        clickHandler(e){
+            e.preventDefault();
+    
+            const $target = e.currentTarget;
+            const $content = $target.nextElementSibling;
+            
+            if($content.style.display === 'block'){
+                $content.style.display = 'none';
+            } else {
+                $content.style.display ='block';
+            }
+        }
+    }
+
+    const fuckingAccordion = new Accordion({
+        hookName: ''
+    });
+
+
+
+ 
+
+})();
